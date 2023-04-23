33d2f37f4e7c1b59aa88736417899be012200cfe
krushna Durole <krushnadurole2000ad@gmail.com>
2023-04-24 01:07:39 +0530

    update

--- Models/User.js
+++ Models/User.js
@@ -1,30 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose=require('mongoose');
 const { Schema } = mongoose;
-
-const UserSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-        
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    contactNum: {
-        type: String,
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    }
+const UserSchema=new mongoose.Schema({
+      name:{
+          type:String,
+          required:true
+      },
+      email:{
+          type:String,
+          required:true,
+          unique:true
+      },
+      password:{
+         type:String,
+         required:true
+      },
+      date:{
+          type:Date,
+          default:Date.now
+      }
 });
 
-const User = mongoose.model('user', UserSchema);
+const User=mongoose.model('user',UserSchema);
+module.exports=User
 // User.createIndexes();
-module.exports = User
